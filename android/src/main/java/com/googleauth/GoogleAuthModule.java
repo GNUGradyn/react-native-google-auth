c4d0d0fbd9b9b9f17f6cb61f537f51e28922becd
Gradyn Wursten <me@gradyn.com>
2024-06-24 16:01:07 -0600

    work

--- android/src/main/java/com/googleauth/GoogleAuthModule.java
+++ android/src/main/java/com/googleauth/GoogleAuthModule.java
@@ -1,19 +1,29 @@
 package com.googleauth;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.credentials.CredentialManager;
+import androidx.credentials.GetCredentialRequest;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
+import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
 
 @ReactModule(name = GoogleAuthModule.NAME)
 public class GoogleAuthModule extends ReactContextBaseJavaModule {
   public static final String NAME = "GoogleAuth";
 
+  CredentialManager credentialManager;
+
   public GoogleAuthModule(ReactApplicationContext reactContext) {
+
     super(reactContext);
+
+    credentialManager = CredentialManager.create(getCurrentActivity());
+
   }
 
   @Override
@@ -22,11 +32,18 @@ public class GoogleAuthModule extends ReactContextBaseJavaModule {
     return NAME;
   }
 
-
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
   @ReactMethod
-  public void multiply(double a, double b, Promise promise) {
-    promise.resolve(a * b);
+  public void SignInWithGoogle(String clientId, @Nullable String nonce,
+                               boolean filterByAuthorizedAccounts, boolean requestVerifiedPhoneNumber,
+                               boolean autoSelectEnabled, Promise promise) {
+    var googleIdOption = new GetGoogleIdOption.Builder()
+      .setServerClientId(clientId)
+      .setNonce(nonce)
+      .setFilterByAuthorizedAccounts(filterByAuthorizedAccounts)
+      .setRequestVerifiedPhoneNumber(requestVerifiedPhoneNumber)
+      .setAutoSelectEnabled(autoSelectEnabled)
+      .build();
+
+    credentialManager.getCredentialAsync(getCurrentActivity(), googleIdOption);
   }
 }
