6a3e2d15352096fcfb5bd52a61bdcc4e2402da56
Gradyn Wursten <me@gradyn.com>
2024-06-25 08:42:45 -0600

    work

--- android/src/main/java/com/googleauth/GoogleAuthModule.java
+++ android/src/main/java/com/googleauth/GoogleAuthModule.java
@@ -1,26 +1,37 @@
 package com.googleauth;
 
+import androidx.credentials.exceptions.GetCredentialException;
+import android.os.CancellationSignal;
+
+import androidx.credentials.CredentialManagerCallback;
+import androidx.credentials.GetCredentialRequest;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.credentials.CredentialManager;
-import androidx.credentials.GetCredentialRequest;
+import androidx.credentials.GetCredentialResponse;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.module.annotations.ReactModule;
 import com.google.android.libraries.identity.googleid.GetGoogleIdOption;
+
+import java.util.concurrent.Executors;
 
 @ReactModule(name = GoogleAuthModule.NAME)
 public class GoogleAuthModule extends ReactContextBaseJavaModule {
   public static final String NAME = "GoogleAuth";
 
   CredentialManager credentialManager;
+  ReactContext context;
 
   public GoogleAuthModule(ReactApplicationContext reactContext) {
 
     super(reactContext);
+    context = reactContext;
 
     credentialManager = CredentialManager.create(getCurrentActivity());
 
@@ -44,6 +55,25 @@ public class GoogleAuthModule extends ReactContextBaseJavaModule {
       .setAutoSelectEnabled(autoSelectEnabled)
       .build();
 
-    credentialManager.getCredentialAsync(getCurrentActivity(), googleIdOption);
+    GetCredentialRequest request = new GetCredentialRequest.Builder()
+      .addCredentialOption(googleIdOption)
+      .build();
+
+    credentialManager.getCredentialAsync(
+      context,
+      request,
+      new CancellationSignal(),
+      Executors.newSingleThreadExecutor(),
+      new CredentialManagerCallback<GetCredentialResponse, GetCredentialException>() {
+        @Override
+        public void onResult(@NonNull GetCredentialResponse result) {
+          promise.resolve(result);
+        }
+        @Override
+        public void onError(@NonNull GetCredentialException error) {
+          promise.reject(error);
+        }
+      }
+      );
   }
 }
