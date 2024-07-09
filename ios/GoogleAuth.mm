a0c8c9dcb1aadef4c2a3f1c4252a4312b379e861
Gradyn Wursten <me@gradyn.com>
2024-07-09 14:13:35 -0600

    work

--- ios/GoogleAuth.mm
+++ ios/GoogleAuth.mm
@@ -2,9 +2,7 @@
 
 @interface RCT_EXTERN_MODULE(GoogleAuth, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(signInWithGoogle:(NSString *)clientID hostedDomainFilter:(NSString *)hostedDomain nonce:(NSString *)nonce withResolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
 
 + (BOOL)requiresMainQueueSetup
 {
--- ios/GoogleAuth.swift
+++ ios/GoogleAuth.swift
@@ -1,8 +1,37 @@
+import Foundation
+import GoogleSignIn
+import React
+
 @objc(GoogleAuth)
 class GoogleAuth: NSObject {
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+    @objc(signInWithGoogle:hostedDomainFilter:nonce:withResolver:withRejecter:)
+    func signInWithGoogle(clientID: String, hostedDomainFilter: String?, nonce: String?, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
+        let gidConfig = GIDConfiguration(clientID: clientID)
+        
+        guard let presentingVC = getCurrentViewController() else {
+          reject("PRESENTING_ERROR", "Unable to find a presenting view controller", nil)
+          return
+        }
+        
+        let signInConfig = GIDConfiguration(clientID: clientID, serverClientID: nil, hostedDomain: hostedDomainFilter, openIDRealm: nil)
+        GIDSignIn.sharedInstance.configuration = signInConfig
+        GIDSignIn.sharedInstance.signIn(withPresenting: presentingVC, hint: nil, additionalScopes: nil, nonce: nonce)
+    }
+    
+    private func getCurrentViewController() -> UIViewController? {
+        guard let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene,
+              let rootViewController = scene.windows.first(where: { $0.isKeyWindow })?.rootViewController else {
+          return nil
+        }
+
+        var currentViewController = rootViewController
+        while let presentedViewController = currentViewController.presentedViewController {
+          currentViewController = presentedViewController
+        }
+
+        return currentViewController
+      }
+    
+    
 }
